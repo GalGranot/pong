7abd752f0e939b631655e72a7a7aa08e25a325b7
galag <gal.granot95@gmail.com>
2026-03-03 10:15:45 +0200

    Paddle stopping at walls

--- Assets/PaddleCtrlr.cs
+++ Assets/PaddleCtrlr.cs
@@ -1,3 +1,4 @@
+using NUnit.Framework.Constraints;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.InputSystem.Controls;
@@ -7,24 +8,35 @@ public class PaddleCtrlr : MonoBehaviour {
     public Key down_key;
     public Key up_key;
     public Rigidbody2D rb;
+    public float bounce_strength;
     float move;
     
     void Start() {
-        rb.bodyType = RigidbodyType2D.Kinematic;
+        // rb.bodyType = RigidbodyType2D.Kinematic;
     }
 
     void OnValidate() {
-        Debug.Assert(speed > 0f);       
+        Debug.Assert(speed > 0f);
+        Debug.Assert(bounce_strength > 0f);
     }
 
     void Update() {
         var y = transform.position.y;
         move = 0f;
-        if(Keyboard.current[up_key].isPressed && y < gm.upper_vertical_border) move += 1f;
-        if(Keyboard.current[down_key].isPressed && y > -gm.upper_vertical_border) move -= 1f;
+            if(Keyboard.current[up_key].isPressed) move += 1f;
+            if(Keyboard.current[down_key].isPressed) move -= 1f;
     }
 
     void FixedUpdate() {
         rb.MovePosition(rb.position + Vector2.up * speed * move * Time.fixedDeltaTime);
+    }
+
+    void OnCollisionEnter2D(Collision2D collision) {
+        //! FIXME add some cool paddle bounce of walls as a perk?
+        // Debug.Log("Paddle collision");
+        // if(collision.gameObject.CompareTag("BorderWalls")) {
+        //     float bounce_dir = transform.position.y > 0 ? -1f : 1f;
+        //     rb.AddForce(transform.up * bounce_dir * bounce_strength);
+        // }
     }
 }
