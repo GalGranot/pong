44f1450d544f18e5b25e2bbce00b8527c02bc878
galag <gal.granot95@gmail.com>
2026-03-06 14:07:56 +0200

    Clean events everywhere

--- Assets/CountdownText.cs
+++ Assets/CountdownText.cs
@@ -2,10 +2,19 @@ using TMPro;
 using UnityEngine;
 
 public class CountdownText : MonoBehaviour {
+    TextMeshProUGUI text;
+    
     void Start() {
-        GameManager
-            .Instance
-            .countdown_text = GetComponent<TextMeshProUGUI>();
-        gameObject.SetActive(false);
+        text = GetComponent<TextMeshProUGUI>();
+        if(!text) throw new MissingComponentException();
+        text.enabled = false;
+    }
+
+    void OnEnable() => GameManager.on_countdown_tick += UpdateText;
+    void OnDisable() => GameManager.on_countdown_tick -= UpdateText;
+
+    void UpdateText(string s) {
+        text.enabled = true;
+        text.text = s;
     }
 }
--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -12,11 +12,11 @@ public class GameManager : MonoBehaviour {
     public float bottom_border = -4.2f;
     int score = 0;
     public int Score => score;
-    public TextMeshProUGUI countdown_text;
 
     public static GameManager Instance;
     public static Action<int> on_score_change;
     public static Action on_move_to_game;
+    public static Action<string> on_countdown_tick;
 
     /*=============================================================================
     * Unity Callbacks 
@@ -57,40 +57,14 @@ public class GameManager : MonoBehaviour {
     }
 
     public async Task DoCountdown() {
-        countdown_text.text = "Use A/S to move";
-        countdown_text.gameObject.SetActive(true);
+        on_countdown_tick?.Invoke("Use A/S to move");
+        await Task.Delay(4000);
 
         for (int i = 3; i > 0; i--) {
-
+            on_countdown_tick?.Invoke(i.ToString());
+            await Task.Delay(1000);
         }
-
-        await Task.Delay(3000);
-        countdown_text.text = "3";
-        await Task.Delay(1000);
-
-        countdown_text.text = "2";
-        await Task.Delay(1000);
-
-        countdown_text.text = "1";
-        await Task.Delay(1000);
-
-        countdown_text.text = "GO!";
+        on_countdown_tick?.Invoke("GO!");
         await Task.Delay(500);
-        countdown_text.gameObject.SetActive(false);
     }
-
-    // public async void DisableMainMenuText() {
-    //     on_move_to_game?.Invoke();
-    //     await DoCountdown();
-    // }
-
-    // public async Task AsyncDisableMainMenuText() {
-    //     var main_menu_text = GameObject.FindWithTag("MainMenuText");
-    //     if (main_menu_text is null) {
-    //         Debug.Log("Game manager could not find main menu text");
-    //     }
-    //     Destroy(main_menu_text);
-    //     await DoCountdown();
-    // }
-
 }
