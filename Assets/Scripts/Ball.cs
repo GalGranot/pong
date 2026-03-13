9d57e541547f931c6d78b5fc25359e9936e124fd
galag <gal.granot95@gmail.com>
2026-03-13 12:26:06 +0200

    Ball collisions with paddle solid

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -1,13 +1,25 @@
+using System;
 using UnityEngine;
 
 public class Ball : MonoBehaviour {
     [SerializeField] Rigidbody2D rb;
     [SerializeField] float speed;
     [SerializeField] float angle_factor;
+    [SerializeField] float max_velocity_magnitude;
+    [SerializeField] float min_y_speed;
 
     void Start() {
         print("Ball using initial set speed - fix this later");
         rb.linearVelocity = new Vector2(7f, 8f);
+    }
+
+    void FixedUpdate() {
+        rb.linearVelocity = Vector2.ClampMagnitude(rb.linearVelocity, max_velocity_magnitude);
+        if(MathF.Abs(rb.linearVelocity.y) < min_y_speed) {
+            var v = rb.linearVelocity;
+            v.y = min_y_speed * MathF.Sign(v.y);
+            rb.linearVelocity = v;
+        }
     }
 
     void OnCollisionEnter2D(Collision2D collision) {
@@ -16,6 +28,7 @@ public class Ball : MonoBehaviour {
             var v = rb.linearVelocity;
             float hit_position = transform.position.y - other.transform.position.y; 
             v.y *= hit_position * angle_factor;
+            v.x *= hit_position * angle_factor;
             rb.linearVelocity = v;
         }
     }
