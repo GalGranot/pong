283469896d4045164a549d04b83ccea88464f7d3
galag <gal.granot95@gmail.com>
2026-03-13 12:10:45 +0200

    Ball jumping from paddle solid - need to fix weird bounce from walls

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class Ball : MonoBehaviour {
     [SerializeField] Rigidbody2D rb;
     [SerializeField] float speed;
+    [SerializeField] float angle_factor;
 
     void Start() {
         print("Ball using initial set speed - fix this later");
@@ -11,6 +12,11 @@ public class Ball : MonoBehaviour {
 
     void OnCollisionEnter2D(Collision2D collision) {
         GameObject other = collision.gameObject;
-        print($"ball collision with {other.tag}");
+        if(other.CompareTag("Paddle")) {
+            var v = rb.linearVelocity;
+            float hit_position = transform.position.y - other.transform.position.y; 
+            v.y *= hit_position * angle_factor;
+            rb.linearVelocity = v;
+        }
     }
 }
