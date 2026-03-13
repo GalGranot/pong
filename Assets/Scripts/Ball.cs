ab4e802e30c6155eb7506cf74d49fc954fbd9c0f
galag <gal.granot95@gmail.com>
2026-03-13 15:18:39 +0200

    Paddle stopping at walls correctly

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -13,15 +13,6 @@ public class Ball : MonoBehaviour {
         rb.linearVelocity = new Vector2(7f, 8f);
     }
 
-    void FixedUpdate() {
-        // rb.linearVelocity = Vector2.ClampMagnitude(rb.linearVelocity, max_velocity_magnitude);
-        // if(MathF.Abs(rb.linearVelocity.y) < min_y_speed) {
-        //     var v = rb.linearVelocity;
-        //     v.y = min_y_speed * MathF.Sign(v.y);
-        //     rb.linearVelocity = v;
-        // }
-    }
-
     void OnTriggerEnter2D(Collider2D collision) {
         if(collision.gameObject.CompareTag("Paddle")) {
             PaddleCollision(collision);
@@ -32,13 +23,11 @@ public class Ball : MonoBehaviour {
     }
 
     void PaddleCollision(Collider2D collision) {
-        print($"collision, v = {rb.linearVelocity}"); 
         var v = rb.linearVelocity;
         if(v.y > 0) return;
 
         float paddle_width = collision.GetComponent<Collider2D>().bounds.size.x;
         float hit_offset = (transform.position.x - collision.transform.position.x) / (paddle_width / 2);
-        print($"hit offset = {hit_offset}");
         if(
             (v.x < 0 && hit_offset > 0) || // ball moving left and hit right of paddle
             (v.x > 0 && hit_offset < 0) // ball moving right and hit left of paddle
--- Assets/Scripts/Paddle.cs
+++ Assets/Scripts/Paddle.cs
@@ -1,10 +1,10 @@
+using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 public class Paddle : MonoBehaviour {
     [SerializeField] Rigidbody2D rb;
-    
-
-    [Range(3f, 10f), SerializeField] float speed;
+    [Range(3f, 20f), SerializeField] float speed;
+    [SerializeField] float x_bound;
     float move = 0f;
 
     void Update() {
@@ -15,6 +15,8 @@ public class Paddle : MonoBehaviour {
 
     void FixedUpdate() {
         if(move == 0f) return;
-        rb.MovePosition(rb.position + move * speed * Time.fixedDeltaTime * Vector2.right);
+        Vector2 new_position = rb.position + move * speed * Time.fixedDeltaTime * Vector2.right;
+        new_position.x = Math.Clamp(new_position.x, -x_bound, x_bound);
+        rb.MovePosition(new_position);
     }
 }
