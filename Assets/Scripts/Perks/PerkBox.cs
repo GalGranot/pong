749dff33ecdbe061f53b93303cefc0e01a07a5ff
galag <gal.granot95@gmail.com>
2026-03-14 13:06:39 +0200

    boxes deleting when exiting

--- Assets/Scripts/Perks/PerkBox.cs
+++ Assets/Scripts/Perks/PerkBox.cs
@@ -7,4 +7,14 @@ public class PerkBox : MonoBehaviour {
         Vector2 new_position = rb.position + fall_speed * Time.fixedDeltaTime * Vector2.down;
         rb.position = new_position;
     }
+
+    void OnTriggerEnter2D(Collider2D collision) {
+        var other = collision.gameObject;
+        if(other.CompareTag("Paddle")) {
+            print("perk - paddle");
+        }
+        else if(other.CompareTag("DeadZone")) {
+            Destroy(gameObject);
+        }
+    }
 }
